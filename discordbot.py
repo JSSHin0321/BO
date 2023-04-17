f5a2e706c0e32813cfc0add2b72e85062d82bc54
JSSHin0321 <130244820+JSSHin0321@users.noreply.github.com>
2023-04-18 00:14:21 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -10,6 +10,23 @@ TOKEN = os.environ['TOKEN']
 
 client = discord.Client()
 
+
+boss_list = {
+    '제니나': {
+        'name': '제니나',
+        'level': 47,
+        'location': 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbS6EiX%2FbtsaibxQI5N%2FeL7dfmI4SnKL5hI6rDXurK%2Fimg.png'
+    },
+    '수호자': {
+        'name': '수호자',
+        'level': 47,
+        'location': 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbmjUmZ%2FbtsakfT1ktr%2FhSqjg4Pz9hKOkg3dYUJ540%2Fimg.png'
+    }
+}
+
+
+
+
 @client.event
 async def on_ready():
     print(f'Logged in as {client.user}.')
@@ -18,12 +35,21 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-
+       
     if message.content == '안녕':
         await hello(message)
+        
+    if message.content == '보스':
+        await print_boss_list(message)
+        
+async def print_boss_list(message):
+    boss_list_str = "보스 리스트:\n"
+    for boss in boss_list.values():
+        boss_list_str += f"{boss['name']} (Lv. {boss['level']})\n"
+    await message.channel.send(boss_list_str)
 
-async def hello(message):
-    await message.channel.send('안녕하세요!')
+    
+    
 
 
 
