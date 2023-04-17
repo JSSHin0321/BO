729ce43b86657c0f06cc8c098c0ca65c0dbead9f
JSSHin0321 <130244820+JSSHin0321@users.noreply.github.com>
2023-04-18 00:19:55 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -15,21 +15,16 @@ boss_list = {
     '제니나': {
         'name': '제니나',
         'level': 47,
-        'location': 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbS6EiX%2FbtsaibxQI5N%2FeL7dfmI4SnKL5hI6rDXurK%2Fimg.png'
+        'location': 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbS6EiX%2FbtsaibxQI5N%2FeL7dfmI4SnKL5hI6rDXurK%2Fimg.png',
+        'regen_time': '3시간'
     },
     '수호자': {
         'name': '수호자',
         'level': 47,
-        'location': 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbmjUmZ%2FbtsakfT1ktr%2FhSqjg4Pz9hKOkg3dYUJ540%2Fimg.png'
+        'location': 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbmjUmZ%2FbtsakfT1ktr%2FhSqjg4Pz9hKOkg3dYUJ540%2Fimg.png',
+        'regen_time': '3시간'
     }
 }
-
-
-
-
-@client.event
-async def on_ready():
-    print(f'Logged in as {client.user}.')
 
 @client.event
 async def on_message(message):
@@ -39,7 +34,7 @@ async def on_message(message):
     if message.content == '안녕':
         await hello(message)
         
-    if message.content == '보스':
+    if message.content == '!보스':
         await print_boss_list(message)
         
 async def print_boss_list(message):
@@ -48,11 +43,8 @@ async def print_boss_list(message):
         boss_list_str += f"{boss['name']} (Lv. {boss['level']})\n"
     await message.channel.send(boss_list_str)
 
+
     
-    
-
-
-
 try:
     client.run(TOKEN)
 except discord.errors.LoginFailure as e:
