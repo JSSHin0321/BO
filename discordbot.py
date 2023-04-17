b294a5c84e92db685cfa4cc583f922dbeee700a4
JSSHin0321 <130244820+JSSHin0321@users.noreply.github.com>
2023-04-18 01:38:25 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -35,22 +35,44 @@ boss_list = {
 async def on_message(message):
     if message.author == client.user:
         return
-        
+
     if message.content == '보스':
         await print_boss_list(message)
-        
+
     for boss_name in boss_list.keys():
         if message.content == f"{boss_name} 컷":
             await boss_kill(message, boss_name)
+        elif message.content.startswith(f"{boss_name} "):
+            input_time_str = message.content.split(' ')[1]
+            await boss_kill(message, boss_name, input_time_str)
 
-async def boss_kill(message, boss_name):
+async def boss_kill(message, boss_name, input_time_str=None):
     tz = pytz.timezone('Asia/Seoul')
-    regen_time = datetime.datetime.now(tz) + datetime.timedelta(hours=3)
+    now = datetime.datetime.now(tz)
+
+    if input_time_str is None:
+        kill_time = now
+    else:
+        try:
+            input_time = datetime.datetime.strptime(input_time_str, '%H%M')
+        except ValueError:
+            await message.channel.send(f"{boss_name} : 입력한 시간이 유효하지 않습니다.")
+            return
+
+        kill_time = datetime.datetime(now.year, now.month, now.day, input_time.hour, input_time.minute, tzinfo=tz)
+
+        if now >= kill_time:
+            kill_time += datetime.timedelta(days=1)
+
+    regen_time = kill_time + datetime.timedelta(hours=3)
     regen_time_str = regen_time.strftime("%H:%M:%S")
-    
-    boss_list[boss_name]['last_kill_time'] = regen_time
-    
+
+    boss_list[boss_name]['last_kill_time'] = kill_time
+
     await message.channel.send(f"{boss_name} Kill. {boss_name}는 {regen_time_str}에 다시 출현합니다.")
+
+    # Update boss list
+    await print_boss_list(message)
 
 
 async def print_boss_list(message):
