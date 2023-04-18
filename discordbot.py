44db60793f238756eb94718e691b15b331d8228d
JSSHin0321 <130244820+JSSHin0321@users.noreply.github.com>
2023-04-19 02:11:15 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,9 +1,11 @@
 from cmath import log
 from distutils.sysconfig import PREFIX
 import discord
-import datetime
 from dotenv import load_dotenv
 import os
+import datetime
+import pytz
+
 load_dotenv()
 
 PREFIX = os.environ['PREFIX']
@@ -28,61 +30,74 @@ boss_list = {
     }
 }
 
+
+async def cut_command(message, boss_name):
+    if boss_name not in boss_list:
+        return
+
+    boss = boss_list[boss_name]
+    kill_time = message.created_at
+    boss['last_kill_time'] = kill_time
+    await message.channel.send(f'{boss_name} killed at {kill_time.strftime("%H:%M:%S")}.')
+
+async def time_command(message, boss_name, kill_time_str):
+    if boss_name not in boss_list:
+        return
+
+    boss = boss_list[boss_name]
+    try:
+        kill_time = datetime.datetime.strptime(kill_time_str, "%H%M")
+        boss['last_kill_time'] = kill_time
+        await message.channel.send(f'{boss_name} killed at {kill_time.strftime("%H:%M:%S")}.')
+    except ValueError:
+        await message.channel.send('Invalid time format. Please use the format "HHMM".')
+
+async def boss_command(message):
+    boss_list_str = "Boss List\n"
+    sorted_bosses = sorted(boss_list.values(), key=lambda b: b['last_kill_time'] + datetime.timedelta(hours=int(b['regen_time'][:-2])) if b['last_kill_time'] else None)
+
+    for b in sorted_bosses:
+        if b['last_kill_time']:
+            respawn_time = b['last_kill_time'] + datetime.timedelta(hours=int(b['regen_time'][:-2]))
+            boss_list_str += f"{b['name']} (Lv. {b['level']}) => {respawn_time.strftime('%H:%M:%S')}\n"
+        else:
+            boss_list_str += f"{b['name']} (Lv. {b['level']}) => No kill history\n"
+
+    await message.channel.send(boss_list_str)
+
+async def reset_command(message, boss_name):
+    if boss_name not in boss_list:
+        return
+
+    boss = boss_list[boss_name]
+    boss['last_kill_time'] = None
+    await message.channel.send(f'{boss_name} kill history has been reset.')
+
+COMMANDS = {
+    '컷': cut_command,
+    '시간': time_command,
+    '보스': boss_command,
+    '초기화': reset_command
+}
+
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
     content = message.content.split()
-    if len(content) < 1:
+    if not content:
         return
 
     command = content[0]
-
-    if command in ['컷', '시간', '초기화']:
-        if len(content) < 2:
-            return
-        boss_name = content[1]
-
-        if boss_name not in boss_list:
-            return
-
-        boss = boss_list[boss_name]
-
-        if command == '컷':
-            kill_time = message.created_at
-            boss['last_kill_time'] = kill_time
-            await message.channel.send(f'{boss_name} killed at {kill_time.strftime("%H:%M:%S")}.')
-
-        elif command == '시간':
-            if len(content) < 3:
-                return
-            kill_time_str = content[2]
-            try:
-                kill_time = datetime.datetime.strptime(kill_time_str, "%H%M")
-                boss['last_kill_time'] = kill_time
-                await message.channel.send(f'{boss_name} killed at {kill_time.strftime("%H:%M:%S")}.')
-
-            except ValueError:
-                await message.channel.send('Invalid time format. Please use the format "HHMM".')
-
-        elif command == '초기화':
-            boss['last_kill_time'] = None
-            await message.channel.send(f'{boss_name} kill history has been reset.')
-
-    elif command == '보스':
-        boss_list_str = "Boss List\n"
-        sorted_bosses = sorted(boss_list.values(), key=lambda b: b['last_kill_time'] + datetime.timedelta(hours=int(b['regen_time'][:-2])) if b['last_kill_time'] else None)
+    if command in COMMANDS:
+        await COMMANDS[command](message, *content[1:])
         
-        for b in sorted_bosses:
-            if b['last_kill_time']:
-                respawn_time = b['last_kill_time'] + datetime.timedelta(hours=int(b['regen_time'][:-2]))
-                boss_list_str += f"{b['name']} (Lv. {b['level']}) => {respawn_time.strftime('%H:%M:%S')}\n"
-            else:
-                boss_list_str += f"{b['name']} (Lv. {b['level']}) => No kill history\n"
-
-        await message.channel.send(boss_list_str)
-
+        
+        
+        
+        
 try:
     client.run(TOKEN)
 except discord.errors.LoginFailure as e:
