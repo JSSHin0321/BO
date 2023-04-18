d5dada1efa88b775657ab1f1888ece793e0844e2
JSSHin0321 <130244820+JSSHin0321@users.noreply.github.com>
2023-04-19 00:37:34 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -85,7 +85,7 @@ async def print_boss_list(message):
         next_spawn_time_str = " "
         if boss['last_kill_time']:
             next_spawn_time = boss['last_kill_time'] + datetime.timedelta(hours=3)
-            next_spawn_time_str = next_spawn_time.strftime("%H:%M:%S")
+            next_spawn_time_str = next_spawn_time.strftime("%Y-%m-%d %H:%M:%S")
         boss_list_str += f"{boss['name']} (Lv. {boss['level']}) => {next_spawn_time_str}\n"
     boss_list_str += "```"
     await message.channel.send(boss_list_str)
