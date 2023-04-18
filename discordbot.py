344ea1e31787b10121888becb7df41c323d9b5a9
JSSHin0321 <130244820+JSSHin0321@users.noreply.github.com>
2023-04-19 01:54:24 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,10 +1,9 @@
+from cmath import log
+from distutils.sysconfig import PREFIX
 import discord
+import datetime
 from dotenv import load_dotenv
 import os
-from datetime
-import datetime, timedelta
-import pytz
-
 load_dotenv()
 
 PREFIX = os.environ['PREFIX']
@@ -29,60 +28,55 @@ boss_list = {
     }
 }
 
-# 보스 kill 시간 저장 및 출력
-def save_kill_time(boss_name, kill_time):
-    boss_list[boss_name]['last_kill_time'] = kill_time
-    print(f'{boss_name}의 마지막 처치 시간: {kill_time}')
-
-# 보스 출현 예상 시간 계산 및 출력
-def print_respawn_time(boss_name):
-    regen_time = boss_list[boss_name]['regen_time']
-    last_kill_time = boss_list[boss_name]['last_kill_time']
-    if last_kill_time is None:
-        print(f'{boss_name}은(는) 처치 기록이 없습니다.')
-        return
-    respawn_time = last_kill_time + timedelta(hours=int(regen_time[0]))
-    print(f'{boss_name}의 출현 예상 시간: {respawn_time.strftime("%Y-%m-%d %H:%M:%S")}')
-
-# 보스 리스트 출력 및 정렬
-def print_boss_list():
-    print('보스 리스트')
-    print('----------')
-    bosses = sorted(boss_list.values(), key=lambda x: x['last_kill_time'] + timedelta(hours=int(x['regen_time'][0])))
-    for boss in bosses:
-        respawn_time = None
-        if boss['last_kill_time'] is not None:
-            respawn_time = boss['last_kill_time'] + timedelta(hours=int(boss['regen_time'][0]))
-        print(f"{boss['name']}: 출현 예상 시간 {respawn_time.strftime('%Y-%m-%d %H:%M:%S') if respawn_time else '미정'}")
-
-# 보스 kill 기록 초기화
-def reset_kill_time(boss_name):
-    boss_list[boss_name]['last_kill_time'] = None
-    print(f'{boss_name}의 처치 기록이 초기화되었습니다.')
-
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith(PREFIX):
-        command = message.content[len(PREFIX):
+    content = message.content.split()
+    if len(content) < 2:
+        return
 
-    if command.startswith('보스이름 '):
-        boss_name = command.split()[1]
-        if boss_name not in boss_list:
-            await message.channel.send(f'{boss_name}은(는) 등록되지 않은 보스입니다.')
-        else:
-            if command.endswith(' 컷'):
-                save_kill_time(boss_name, message.created_at.astimezone(pytz.timezone('Asia/Seoul')))
-            elif command.endswith(' 시간'):
-                print_respawn_time(boss_name)
-            elif command == '보스':
-                print_boss_list()
-            elif command.endswith(' 초기화'):
-                reset_kill_time(boss_name)
+    command, boss_name = content[:2]
+
+    if boss_name not in boss_list:
+        return
+
+    boss = boss_list[boss_name]
+
+    if command == '컷':
+        kill_time = message.created_at
+        boss['last_kill_time'] = kill_time
+        await message.channel.send(f'{boss_name} killed at {kill_time.strftime("%H:%M:%S")}.')
+
+    elif command == '시간':
+        if len(content) < 3:
+            return
+        kill_time_str = content[2]
+        try:
+            kill_time = datetime.datetime.strptime(kill_time_str, "%H%M")
+            boss['last_kill_time'] = kill_time
+            await message.channel.send(f'{boss_name} killed at {kill_time.strftime("%H:%M:%S")}.')
+
+        except ValueError:
+            await message.channel.send('Invalid time format. Please use the format "HHMM".')
+
+    elif command == '보스':
+        boss_list_str = "Boss List\n"
+        sorted_bosses = sorted(boss_list.values(), key=lambda b: b['last_kill_time'] + datetime.timedelta(hours=int(b['regen_time'][:-2])) if b['last_kill_time'] else None)
+        
+        for b in sorted_bosses:
+            if b['last_kill_time']:
+                respawn_time = b['last_kill_time'] + datetime.timedelta(hours=int(b['regen_time'][:-2]))
+                boss_list_str += f"{b['name']} (Lv. {b['level']}) => {respawn_time.strftime('%H:%M:%S')}\n"
             else:
-                await message.channel.send(f'잘못된 명령어입니다. "{PREFIX}도움말"을 입력해 사용 가능한 명령어를 확인하세요.')
+                boss_list_str += f"{b['name']} (Lv. {b['level']}) => No kill history\n"
+
+        await message.channel.send(boss_list_str)
+
+    elif command == '초기화':
+        boss['last_kill_time'] = None
+        await message.channel.send(f'{boss_name} kill history has been reset.')
 
 try:
     client.run(TOKEN)
