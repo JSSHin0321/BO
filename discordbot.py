d184ac7ce06969b94d779e40a398340383ee3407
JSSHin0321 <130244820+JSSHin0321@users.noreply.github.com>
2023-04-18 11:34:25 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -13,6 +13,7 @@ TOKEN = os.environ['TOKEN']
 
 client = discord.Client()
 
+# Boss list
 boss_list = {
     '제니나': {
         'name': '제니나',
@@ -30,7 +31,7 @@ boss_list = {
     }
 }
 
-
+# Event handler for receiving messages
 @client.event
 async def on_message(message):
     if message.author == client.user:
@@ -46,10 +47,12 @@ async def on_message(message):
             input_time_str = message.content.split(' ')[1]
             await boss_kill(message, boss_name, input_time_str)
 
+# Boss kill handler
 async def boss_kill(message, boss_name, input_time_str=None):
     tz = pytz.timezone('Asia/Seoul')
     now = datetime.datetime.now(tz)
 
+    # If no input time is provided, use the current time as the kill time
     if input_time_str is None:
         kill_time = now
     else:
@@ -61,42 +64,24 @@ async def boss_kill(message, boss_name, input_time_str=None):
 
         kill_time = datetime.datetime(now.year, now.month, now.day, input_time.hour, input_time.minute, tzinfo=tz)
 
+        # If the input time is in the past, assume it was from yesterday
         if now >= kill_time:
             kill_time += datetime.timedelta(days=1)
 
+    # Calculate the respawn time based on a 3-hour respawn time
     regen_time = kill_time + datetime.timedelta(hours=3)
     regen_time_str = regen_time.strftime("%H:%M:%S")
 
+    # Update the last kill time for the boss
     boss_list[boss_name]['last_kill_time'] = kill_time
 
+    # Send a message to the channel indicating the kill time and next respawn time
     await message.channel.send(f"{boss_name} Kill. {boss_name}는 {regen_time_str}에 다시 출현합니다.")
 
-    # Update boss list
+    # Update the boss list display
     await print_boss_list(message)
-
-
-
-
-async def print_boss_list(message):
-    sorted_boss_list = sorted(boss_list.values(), key=lambda x: x['last_kill_time'] + datetime.timedelta(hours=3) if x['last_kill_time'] else datetime.datetime.max)
-    boss_list_str = "```보스 리스트:\n"
-    for boss in sorted_boss_list:
-        next_spawn_time_str = " "
-        if boss['last_kill_time']:
-            next_spawn_time = boss['last_kill_time'] + datetime.timedelta(hours=3)
-            next_spawn_time_str = next_spawn_time.strftime("%H:%M:%S")
-        boss_list_str += f"{boss['name']} (Lv. {boss['level']}) => {next_spawn_time_str}\n"
-    boss_list_str += "```"
-    await message.channel.send(boss_list_str)
-
-
-
-
-
-
-
-
-
+    
+    
 try:
     client.run(TOKEN)
 except discord.errors.LoginFailure as e:
