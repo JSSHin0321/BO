e87c78ff7446c48d425d4726999885a30f56a7c7
JSSHin0321 <130244820+JSSHin0321@users.noreply.github.com>
2023-04-18 12:56:02 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,5 +1,3 @@
-from cmath import log
-from distutils.sysconfig import PREFIX
 import discord
 from dotenv import load_dotenv
 import os
@@ -19,19 +17,18 @@ boss_list = {
         'level': 47,
         'location': 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbS6EiX%2FbtsaibxQI5N%2FeL7dfmI4SnKL5hI6rDXurK%2Fimg.png',
         'regen_time': '3시간',
-        'last_kill_time': None
+        'last_kill_time': None,
+        'next_spawn_time': None
     },
     '수호자': {
         'name': '수호자',
         'level': 47,
         'location': 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbmjUmZ%2FbtsakfT1ktr%2FhSqjg4Pz9hKOkg3dYUJ540%2Fimg.png',
         'regen_time': '3시간',
-        'last_kill_time': None
+        'last_kill_time': None,
+        'next_spawn_time': None
     }
 }
-
-
-
 
 @client.event
 async def on_message(message):
@@ -70,20 +67,23 @@ async def boss_kill(message, boss_name, input_time_str=None):
     regen_time_str = regen_time.strftime("%H:%M:%S")
 
     boss_list[boss_name]['last_kill_time'] = kill_time
+    boss_list[boss_name]['next_spawn_time'] = regen_time
 
     await message.channel.send(f"{boss_name} Kill. {boss_name}는 {regen_time_str}에 다시 출현합니다.")
 
+    # Update boss list
+    await print_boss_list(message)
+
 async def print_boss_list(message):
-    boss_list_sorted = sorted(boss_list.values(), key=lambda x: x['last_kill_time'] + datetime.timedelta(hours=3) if x['last_kill_time'] else datetime.datetime.max)
     boss_list_str = "```보스 리스트:\n"
-    for boss in boss_list_sorted:
-        next_spawn_time_str = " "
-        if boss['last_kill_time']:
-            next_spawn_time = boss['last_kill_time'] + datetime.timedelta(hours=3)
-            next_spawn_time_str = next_spawn_time.strftime("%H:%M:%S")
-        boss_list_str += f"{boss['name']} (Lv. {boss['level']}) => {next_spawn_time_str}\n"
+    for boss in sorted(boss_list.values(), key=lambda x: x['next_spawn_time'] if x['next_spawn_time'] else datetime.datetime.max):
+        next_spawn_time_str = "미정"
+        if boss['next_spawn_time']:
+            next_spawn_time_str = boss['next_spawn_time'].strftime("%m/%d %H:%M")
+        boss_list_str += f"{boss['name']} (Lv. {boss['level']}) => 예상 출현 시간: {next_spawn_time_str}\n"
     boss_list_str += "```"
     await message.channel.send(boss_list_str)
+
 
 
 
