8b4a2684142066e695aa24c405f58746b1b1925d
JSSHin0321 <130244820+JSSHin0321@users.noreply.github.com>
2023-04-19 14:44:18 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -73,6 +73,9 @@ async def on_message(message):
         if len(parts) == 2 and parts[1] == '컷':
             boss['last_kill_time'] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
             await message.channel.send(f"{boss['name']}의 last kill time이 갱신되었습니다.")
+        elif len(parts) == 1:
+            location_link = boss['location']
+            await message.channel.send(location_link)
         elif parts[1].isdigit() and len(parts[1]) == 4:
             kst = pytz.timezone('Asia/Seoul')
             now_kst = datetime.datetime.now(kst)
