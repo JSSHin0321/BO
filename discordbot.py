93ee444cfcefead3b8c555bf470296b349d2f808
JSSHin0321 <130244820+JSSHin0321@users.noreply.github.com>
2023-04-20 05:31:04 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -173,26 +173,21 @@ async def on_message(message):
     if command == '보스':
         boss_info_list = []
         for boss in sorted(boss_list.values(), key=lambda x: x['last_kill_time'] or '9999-99-99 99:99:99'):
-            name = f"{boss['name']}"
-            name = name[:8] if len(name) > 8 else name
-            level = f"{boss['level']}".rjust(3, ' ')
             if boss['last_kill_time'] is None:
-                expected_spawn_time = ' '.rjust(8, ' ')
+                expected_spawn_time = ' '
             else:
                 last_kill_time = datetime.datetime.strptime(boss['last_kill_time'], '%Y-%m-%d %H:%M:%S.%f').replace(tzinfo=pytz.utc)
                 regen_time = datetime.timedelta(hours=int(boss['regen_time'][0]))
                 kst = pytz.timezone('Asia/Seoul')
                 expected_spawn_time = (last_kill_time + regen_time).astimezone(kst)
-                expected_spawn_time = expected_spawn_time.strftime('%H:%M:%S').rjust(8, ' ')
+                expected_spawn_time = expected_spawn_time.strftime('%H:%M:%S')
 
-            boss_info = "{:<8} (Lv. {}) ==> {}".format(name, level, expected_spawn_time)
+            boss_info = f"{boss['name']} (Lv. {boss['level']}) ==> {expected_spawn_time}"
             boss_info_list.append(boss_info)
 
         boss_info_str = "\n".join(boss_info_list)
         boss_embed = discord.Embed(title="보스 정보", description=boss_info_str, color=0x00FF00)
         await message.channel.send(embed=boss_embed)
-
-
 
 
     elif command in boss_list:
